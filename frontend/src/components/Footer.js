d8c1fb53675bce9b8e0ec75f090f29617c0ea17c
alessio rea <alexei.80@hotmail.fr>
2021-02-07 10:38:45 +0100

    added screens

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -1,15 +1,14 @@
 import React from 'react'
-// import { Container, Row, Col } from 'react-bootstrap'
+import { Container, Row, Col } from 'react-bootstrap'
 
 const Footer = () => {
   return (
     <footer>
-      {/* <Container>
+      <Container>
         <Row>
-          <Col className='text-center py-3'>Copyright &copy; ProShop</Col>
+          <Col className='text-center py-3'>Copyright &copy; shopR</Col>
         </Row>
-      </Container> */}
-      <h1>footer</h1>
+      </Container>
     </footer>
   )
 }
