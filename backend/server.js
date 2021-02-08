3bef71e6ac30549dfce50b7b65ba7ce22046cd12
alessio rea <alexei.80@hotmail.fr>
2021-02-08 09:33:45 +0100

    progress but connection to cloud failed

--- backend/server.js
+++ backend/server.js
@@ -31,14 +31,14 @@ const app = express()
 
 
 app.get('/', (req, res) => {
-  res.send('API is running test....')
+  res.send('API is running...')
 })
 
 app.get('/api/products', (req, res) => {
   res.json(products)
 })
 
-app.get('/api/product/:id', (req, res) => {
+app.get('/api/products/:id', (req, res) => {
   const product = products.find((p) => p._id === req.params.id)
   res.json(product)
 })
