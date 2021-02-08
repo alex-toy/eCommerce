3863ea4c9c64175fc3f77dc6913339f87f56dc06
alessio rea <alexei.80@hotmail.fr>
2021-02-08 12:09:47 +0100

    progress bur error

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,53 +1,50 @@
-//import React, { useEffect } from 'react'
-import React, { useState, useEffect } from 'react'
+import React, { useEffect } from 'react'
 // import { Link } from 'react-router-dom'
-// import { useDispatch, useSelector } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
 import { Row, Col } from 'react-bootstrap'
 import Product from '../components/Product'
-// import Message from '../components/Message'
-// import Loader from '../components/Loader'
+import Message from '../components/Message'
+import Loader from '../components/Loader'
 // import Paginate from '../components/Paginate'
 // import ProductCarousel from '../components/ProductCarousel'
 // import Meta from '../components/Meta'
-// import { listProducts } from '../actions/productActions'
+import { listProducts } from '../actions/productActions'
 
 // import products from '../products_and_images/products'
-import axios from 'axios'
+//import axios from 'axios'
 
 const HomeScreen = ({ match }) => {
   // const keyword = match.params.keyword
 
   // const pageNumber = match.params.pageNumber || 1
 
-  // const dispatch = useDispatch()
+  const dispatch = useDispatch()
 
-  // const productList = useSelector((state) => state.productList)
-  // const { loading, error, products, page, pages } = productList
-
-  // useEffect(() => {
-  //   dispatch(listProducts(keyword, pageNumber))
-  // }, [dispatch, keyword, pageNumber])
-
-  const [products, setProducts] = useState([])
+  const productList = useSelector((state) => state.productList)
+  const { loading, error, products } = productList
 
   useEffect(() => {
-    const fetchProducts = async() => {
-      const { data } = await axios.get('/api/products')
-      setProducts(data)
-    }
-    fetchProducts()
-  }, [])
+    dispatch(listProducts())
+  }, [dispatch])
+
 
   return (
     <>
-      <h1>Products</h1>
-      <Row>
-        {products.map(product => (
-          <Col>
-            <Product product={product} />
-          </Col>
-        ))}
-      </Row>
+      <h1>Latest Products</h1>
+      {loading ? (
+        <Loader />
+      ) : error ? (
+        <Message variant='danger'>{error}</Message>
+      ) : (
+        <Row>
+          {products.map(product => (
+            <Col>
+              <Product product={product} />
+            </Col>
+          ))}
+        </Row>
+      )}
+      
 
       {/* <Meta />
       {!keyword ? (
--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -64,7 +64,7 @@ const ProductScreen = ({ history, match }) => {
 
   useEffect(() => {
     const fetchProduct = async() => {
-      const { data } = await axios.get(`/api/product/${match.params.id}`)
+      const { data } = await axios.get(`/api/products/${match.params.id}`)
       setProduct(data)
     }
     fetchProduct()
